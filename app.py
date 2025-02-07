647791adb42158b56e30dc1faa38746e99f70bfa
Ram Mohith <rammohith123@gmail.com>
2025-02-07 11:19:25 +0530

    day2

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
 @app.route("/")
-def Home():
+def home():
     return render_template("index.html")
 
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        message = request.form.get("message")
+        print(message)
+    else:
+        return render_template("predict.html")
 if __name__ == "__main__":
     app.run()
