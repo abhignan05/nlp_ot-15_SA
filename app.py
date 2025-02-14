e6b9faabf1b30f03bd126ee69175820d5359b872
Abhignan Bollam <abhignanbollam@gmail.com>
2025-02-14 10:01:04 +0530

    hlo

--- app.py
+++ app.py
@@ -23,12 +23,21 @@ def predict():
         with open('model.pickle',"rb") as md_file:
             model=pickle.load(md_file)
         pred=model.predict(dt)
-        print(pred)
-        return jsonify({"prediction":str(pred[0])})
+        if pred[0]==1:
+            pred="Positive"
+        elif pred[0]==0:
+            pred="Neutral"
+        else:
+            pred="Negative"
+        
+        return render_template("result.html",prediction=pred)
+        ##print(pred)
+        #return jsonify({"prediction":str(pred[0])})
+        #prediction=str(pred[0])
 
 
 
     else:
         return render_template("predict.html")
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5050)
+    app.run(host="0.0.0.0",port=5151)
